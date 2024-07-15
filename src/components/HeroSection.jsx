5baee704a101383e9fa71120922db2cc50c7d63e
Anna N <nahid.najafi.h@gmail.com>
2024-07-15 14:31:31 +0200

    hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,14 +4,13 @@ import "../index.css";
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <div className="container mx-auto h-full flex justify-end items-center text-white">
-        <div className="hero-content text-right">
+      <div className="container mx-auto h-full flex justify-center items-center text-white">
+        <div className="hero-content text-center">
           <div className="mt-24">
-            {" "}
-            <h1 className="hero-title text-4xl font-bold mb-4 text-center md:text-right">
+            <h1 className="hero-title text-4xl font-bold mb-4">
               New Model Cycle
             </h1>
-            <p className="hero-subtitle text-lg max-w-2xl mb-6 text-center md:text-right">
+            <p className="hero-subtitle text-lg max-w-2xl mb-6">
               It is a long established fact that a reader will be distracted by
               the readable content.
             </p>
