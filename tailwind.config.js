be3109e10edcb87a81238073b3f3ad5b54d8f117
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-15 11:59:40 +0200

    feat: added ContactForm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-        "./index.html",
+    "./index.html",
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        bp: {
+          green: '#355764;',
+          yellow: '#f7c17b',
+          darktext: '#292929',
+          lighttext: '#ffffff'
+        },
+      },
+    },
+    plugins: [],
+  }
 }
-
