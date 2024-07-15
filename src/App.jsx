eac1d8312141792a2e8c40b374ea0267ad860a58
Anna N <nahid.najafi.h@gmail.com>
2024-07-15 11:18:59 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Main from "./components/Main"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
     <>
+    <Navbar/>
       <Header />
       <Main />
       <Footer />
