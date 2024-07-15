71c4c9f22510cf8e22f1c09d863e3d2092c325c3
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-15 13:27:31 +0200

    feat: added FooterForm

--- src/components/FooterForm.jsx
+++ src/components/FooterForm.jsx
@@ -1,9 +1,17 @@
 const FooterForm = () => {
 
     return (
-        <div className="gris place-content-center">
-            Form
-        </div>
+        <form className="grid gap-2">
+            <label className="sr-only" htmlFor="footer-email">
+                Email:
+            </label>
+            <input type="email" name="footer-email" id="footer-email" required placeholder="Enter your email"
+                className="block min-w-full py-2 px-4 placeholder:text-sm"
+            />
+            <button className="px-6 py-2  bg-bp-yellow text-bp-lighttext uppercase">
+                Subscribe
+            </button>
+        </form>
     )
 }
 
