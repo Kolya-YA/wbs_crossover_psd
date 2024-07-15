293d2e9281796e618d0bbea8608187dc99b3cb20
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2024-07-15 13:14:10 +0200

    added FooterAddress component

--- src/components/FooterAddress.jsx
+++ src/components/FooterAddress.jsx
@@ -1,10 +1,35 @@
+import { FiMail, FiMapPin, FiPhoneCall } from "react-icons/fi"
+
 const FooterAddress = () => {
 
+    const address = [
+        "Weiskopffstraße 16 / 17",
+        "12459 Berlin",
+        "Germany"
+    ]
+
     return (
-        <div className="gris place-content-center">
-            Address
+        <div className="grid gap-2">
+            <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
+                <FiMapPin />
+                <div>
+                    {address.map((line, index) => (
+                        <p key={index}>{line}</p>
+                    ))}
+                </div>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
+                <FiPhoneCall />
+                <a href="tel:+4930555789760">+49 30 555789760</a>
+            </div>
+            <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
+                <FiMail />
+                <a href="mailto:crossover@wbscodingschool.com">crossover@wbscodingschool.com</a>
+            </div>
         </div>
     )
 }
 
 export default FooterAddress
+
+
--- src/components/FooterContact.jsx
+++ src/components/FooterContact.jsx
@@ -1,4 +1,4 @@
-import FooterSocial from "./FooteSocial"
+import FooterSocial from "./FooterSocial"
 import FooterAddress from "./FooterAddress"
 import FooterForm from "./FooterForm"
 import FooterMap from "./FooterMap"
@@ -8,7 +8,7 @@ const FooterContact = () => {
     return (
         <div className="grid grid-cols-[2fr_1fr]">
             <FooterMap />
-            <div className="px-4 py-2 bg-bp-green text-bp-lighttext">
+            <div className="grid gap-4 px-4 py-2 bg-bp-green text-bp-lighttext">
                 <FooterAddress />
                 <FooterSocial />
                 <FooterForm />
